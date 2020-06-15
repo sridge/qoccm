2a7b31926f86f51c1fbcb170a5df230eceea1f36
sridge <smr1020@gmail.com>
2020-06-15 15:40:41 -0400

    add master_doc to conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -35,6 +35,8 @@ extensions = ['sphinx.ext.napoleon','sphinx.ext.autodoc']
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
+master_doc = 'index'
+
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
